6fc3ed19bcc97f44df27eaa247ebbb612b91fae4
ketal <k@ketal.icu>
2026-05-05 02:30:54 +0800

    64gram v1.2.1 (TDesktop v6.7.8)

---
+++ Casks/64gram.rb
@@ -0,0 +1,31 @@
+cask "64gram" do
+  version "1.2.1"
+  sha256 "5bc6df26c617bdefe135616a23ae869ca44ce774b6ce65e7a570c02f0a49df2d"
+
+  url "https://github.com/TDesktop-x64/tdesktop/releases/download/v#{version}/64Gram_#{version}_mac.dmg",
+      verified: "github.com/TDesktop-x64/tdesktop"
+  name "64Gram"
+  desc "Unofficial Telegram Desktop client"
+  homepage "https://github.com/TDesktop-x64/tdesktop"
+
+  auto_updates true
+  app "Telegram.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Telegram.app"]
+  end
+
+  uninstall quit: "com.tdesktop.Telegram"
+
+  zap trash: [
+    "~/Library/Application Support/Telegram Desktop",
+    "~/Library/Preferences/com.tdesktop.Telegram.plist",
+    "~/Library/Saved Application State/com.tdesktop.Telegram.savedState",
+  ]
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+end
